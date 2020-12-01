b763389b6446ce0eff61de9743228b1946d49091
HYBB-rash <hyong@hybing.onmicrosoft.com>
2020-12-02 07:56:34 +0800

    完成第三课

--- Lesson3/src/main.cpp
+++ Lesson3/src/main.cpp
@@ -60,6 +60,70 @@ int main(int argc, char** argv) {
 
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
         logSDLError(std::cout, "SDL_Init");
+        SDL_Quit();
         return 1;
     }
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+        logSDLError(std::cout, "SDL_Init");
+        return 1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+            "Lesson2",
+            100, 100,
+            SCREEN_WIDTH, SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN
+            );
+    if (window == nullptr) {
+        logSDLError(std::cout, "CreateWindow");
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(
+            window,
+            -1,
+            SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+            );
+    if (renderer == nullptr) {
+        logSDLError(std::cout, "CreateRenderer");
+        cleanup(window);
+        SDL_Quit();
+        return 1;
+    }
+
+    const std::string resPath = getResourcePath("Lesson3");
+    SDL_Texture *background = loadTexture(resPath + "background.png", renderer);
+    SDL_Texture *image = loadTexture(resPath + "image.png", renderer);
+    if (background == nullptr || image == nullptr) {
+        cleanup(background, image, renderer, window);
+        IMG_Quit();
+        SDL_Quit();
+        return 1;
+    }
+
+    int xTiles = SCREEN_WIDTH / TILE_SIZE;
+    int yTiles = SCREEN_HEIGHT / TILE_SIZE;
+
+    for (int i = 0; i < xTiles * yTiles; ++ i) {
+        int x = i % xTiles;
+        int y = i  / xTiles;
+        renderTexture(background, renderer, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+    }
+
+    int iW, iH;
+    SDL_QueryTexture(image, nullptr, nullptr, &iW, &iH);
+    int x = SCREEN_WIDTH / 2 - iW / 2;
+    int y = SCREEN_HEIGHT / 2 - iH / 2;
+    renderTexture(image, renderer, x, y);
+
+    SDL_RenderPresent(renderer);
+    SDL_Delay(2000);
+
+    cleanup(background, image, renderer, window);
+    IMG_Quit();
+    SDL_Quit();
+
+    return 0;
 }
