46b5c5570eea8a61d674443e1c81e594cf89f75d
HYBB-rash <hyong@hybing.onmicrosoft.com>
2020-12-02 10:51:08 +0800

    Lesson4完成

---
+++ Lesson4/src/main.cpp
@@ -0,0 +1,155 @@
+//
+// Created by hyong on 2020/12/1.
+//
+
+#include <iostream>
+#include <string>
+#include "cleanup.h"
+#include "res_path.h"
+#include "catch_error.h"
+#include <SDL.h>
+#include <SDL_image.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+const int TILE_SIZE = 40;
+
+/**
+* Loads an image into a texture on the rendering device
+* @param file The image file to load
+* @param ren The renderer to load the texture onto
+* @return the loaded texture, or nullptr if something went wrong.
+*/
+SDL_Texture *loadTexture(const std::string &file, SDL_Renderer *ren)
+{
+  SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
+  if (texture == nullptr)
+    logSDLError(std::cout, "LoadTexture");
+  return texture;
+}
+
+/**
+* Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
+* width and height
+* @param tex The source texture we want to draw
+* @param ren The renderer we want to draw to
+* @param x The x coordinate to draw to
+* @param y The y coordinate to draw to
+* @param w The width of the texture to draw
+* @param h The height of the texture to draw
+*/
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h)
+{
+  SDL_Rect dst;
+  dst.x = x;
+  dst.y = y;
+  dst.w = w;
+  dst.h = h;
+  SDL_RenderCopy(ren, tex, nullptr, &dst);
+}
+
+/**
+* Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+* the texture's width and height
+* @param tex The source texture we want to draw
+* @param ren The renderer we want to draw to
+* @param x The x coordinate to draw to
+* @param y The y coordinate to draw to
+*/
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y)
+{
+  int w, h;
+  SDL_QueryTexture(tex, nullptr, nullptr, &w, &h);
+  renderTexture(tex, ren, x, y, w, h);
+}
+
+int main(int argc, char **argv)
+{
+
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+  {
+    logSDLError(std::cout, "SDL_Init");
+    SDL_Quit();
+    return 1;
+  }
+
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+  {
+    logSDLError(std::cout, "SDL_Init");
+    return 1;
+  }
+
+  SDL_Window *window = SDL_CreateWindow(
+      "Lesson2",
+      100, 100,
+      SCREEN_WIDTH, SCREEN_HEIGHT,
+      SDL_WINDOW_SHOWN);
+  if (window == nullptr)
+  {
+    logSDLError(std::cout, "CreateWindow");
+    SDL_Quit();
+    return 1;
+  }
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(
+      window,
+      -1,
+      SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (renderer == nullptr)
+  {
+    logSDLError(std::cout, "CreateRenderer");
+    cleanup(window);
+    SDL_Quit();
+    return 1;
+  }
+
+  const std::string resPath = getResourcePath("Lesson4");
+  SDL_Texture *background = loadTexture(resPath + "background.png", renderer);
+  SDL_Texture *image = loadTexture(resPath + "image.png", renderer);
+  if (background == nullptr || image == nullptr)
+  {
+    cleanup(background, image, renderer, window);
+    IMG_Quit();
+    SDL_Quit();
+    return 1;
+  }
+
+  int xTiles = SCREEN_WIDTH / TILE_SIZE;
+  int yTiles = SCREEN_HEIGHT / TILE_SIZE;
+
+  for (int i = 0; i < xTiles * yTiles; ++i)
+  {
+    int x = i % xTiles;
+    int y = i / xTiles;
+    renderTexture(background, renderer, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+  }
+
+  int iW, iH;
+  SDL_QueryTexture(image, nullptr, nullptr, &iW, &iH);
+  int x = SCREEN_WIDTH / 2 - iW / 2;
+  int y = SCREEN_HEIGHT / 2 - iH / 2;
+
+  SDL_Event e;
+  bool quit = false;
+  while (!quit)
+  {
+    while (SDL_PollEvent(&e))
+    {
+      if (e.type == SDL_QUIT)
+        quit = true;
+      if (e.type == SDL_KEYDOWN)
+        quit = true;
+      if (e.type == SDL_MOUSEBUTTONDOWN)
+        quit = true;
+    }
+    SDL_RenderClear(renderer);
+    renderTexture(image, renderer, x, y);
+    SDL_RenderPresent(renderer);
+  }
+
+  cleanup(background, image, renderer, window);
+  IMG_Quit();
+  SDL_Quit();
+
+  return 0;
+}
